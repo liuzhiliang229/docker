789e21c9dc274196841b12159bd545f1d6ea17ef
M.Liu <liuzhiliangzh@163.com>
2020-05-12 16:07:01 +0800

    update etcd

--- scripts/kubernetes-images.sh
+++ scripts/kubernetes-images.sh
@@ -8,16 +8,16 @@ From_Ali(){
     ali_images=(
 	  	kube-apiserver-v1.17.4
 	  	kube-controller-manager-v1.17.4
-	  	coredns-1.6.2
-	  	etcd-3.3.15
+	  	coredns-1.6.5
+	  	etcd-3.4.3-0
 	  	pause-3.1
 	  	kube-proxy-v1.17.4
 	  	kube-scheduler-v1.17.4
 	       )
     k8s_images=(
     		k8s.gcr.io/pause:3.1
-    		k8s.gcr.io/etcd:3.3.15-0
-    		k8s.gcr.io/coredns:1.6.2
+    		k8s.gcr.io/etcd:3.4.3-0
+    		k8s.gcr.io/coredns:1.6.5
     		k8s.gcr.io/kube-controller-manager:v1.17.4
     		k8s.gcr.io/kube-scheduler:v1.17.4
     		k8s.gcr.io/kube-proxy:v1.17.4
@@ -39,8 +39,8 @@ From_Ali(){
         echo -e "\033[1;32mAdd Tag\033[0m"
         docker tag $registry_url:kube-apiserver-v1.17.4 k8s.gcr.io/kube-apiserver:v1.17.4
         docker tag $registry_url:kube-controller-manager-v1.17.4 k8s.gcr.io/kube-controller-manager:v1.17.4
-        docker tag $registry_url:coredns-1.6.2 k8s.gcr.io/coredns:1.6.2
-        docker tag $registry_url:etcd-3.3.15 k8s.gcr.io/etcd:3.3.15-0
+        docker tag $registry_url:coredns-1.6.5 k8s.gcr.io/coredns:1.6.5
+        docker tag $registry_url:etcd-3.4.3-0 k8s.gcr.io/etcd:3.4.3-0
         docker tag $registry_url:pause-3.1 k8s.gcr.io/pause:3.1
         docker tag $registry_url:kube-proxy-v1.17.4 k8s.gcr.io/kube-proxy:v1.17.4
         docker tag $registry_url:kube-scheduler-v1.17.4 k8s.gcr.io/kube-scheduler:v1.17.4
