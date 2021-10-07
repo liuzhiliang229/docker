1f0efcefda7879c9d61989836c85089cda78d035
mliuz <7516300+mliuz@user.noreply.gitee.com>
2021-10-07 12:06:58 +0800

    1.21.2

--- ch_ver.sh
+++ ch_ver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-old_ver=1.16.3
-newer_ver=1.17.4
+old_ver=1.17.4
+newer_ver=1.21.2
 for i in $(find . -type f | grep Dockerfile) scripts/kubernetes-images.sh
 do
     sed -i "s/$old_ver/$newer_ver/" $i
